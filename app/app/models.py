afb0f7660d391553b68b96dd7b91f2bd9fa44a0d
ppaladug <ppaladug@akamai.com>
2023-05-06 21:11:16 -0700

    Added basic test case

--- app/app/models.py
+++ app/app/models.py
@@ -4,3 +4,8 @@ from cassandra.cqlengine import columns
 class CassandraTestTable(DjangoCassandraModel):
     test_id = columns.UUID(primary_key=True)
     test_name = columns.UUID(primary_key=True)
+    class Meta:
+        app_label = 'app'
+        get_pk_field = 'test_id'
+        #partition_key_fields = 'test_id'
+        #clustering_key_fields = 'test_name'
--- app/app/test.py
+++ app/app/test.py
@@ -1,5 +1,12 @@
-from models import CassandraTestTable
-
-rows = CassandraTestTable.objects.all()
-for row in rows:
-    print(f'ID: {row.test_id} | NAME: {row.test_name}')
+from .models import CassandraTestTable
+from django.views import View
+from django.http import HttpResponse
+from django.test import TestCase
+class BasicCassandraTest(View, TestCase):
+    def get(self, request):
+        rows = CassandraTestTable.objects.all()
+        output = []
+        for row in rows:
+            output.append(f'ID: {row.test_id} | NAME: {row.test_name}')
+            print(f'ID: {row.test_id} | NAME: {row.test_name}')
+        return HttpResponse('\n'.join(output))
